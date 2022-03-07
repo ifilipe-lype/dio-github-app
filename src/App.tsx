d116f3500ba6ed1e100c21da9b1a5fe98c9e0f2d
ifilipe-lype <ifilipe.lype@gmail.com>
2022-03-07 11:22:49 +0100

    feat: setup create-react-app with typescript and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen">
+      <h1 className='text-3xl text-gray-500 font-bold'>Hello, world</h1>
+    </div>
+  );
+}
+
+export default App;
