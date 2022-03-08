a0317c73a62783850c80f57c811056d29be3097e
ifilipe-lype <ifilipe.lype@gmail.com>
2022-03-08 22:00:16 +0100

    feat: adds main app's header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import SearchBar from "./components/search";
 
 function App() {
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      <h1 className='text-3xl text-gray-500 font-bold'>Hello, world</h1>
+    <div className="flex justify-center">
+      <div className="max-w-4xl w-full flex flex-col items-center">
+        <header className="flex items-center justify-between w-full p-4">
+          <div>
+            <h1>DIO-Github-app</h1>
+          </div>
+          <div className="flex max-w-md w-full">
+            <SearchBar />
+          </div>
+        </header>
+      </div>
     </div>
   );
 }
