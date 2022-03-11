08c00c4b00a45f8fd2627cf1330193c384a3ca32
ifilipe-lype <ifilipe.lype@gmail.com>
2022-03-11 12:10:23 +0100

    feat: makes app logo a link to homepage

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import SearchBar from "../search";
 
 export function Header() {
   return (
     <header className="flex items-center justify-between w-full p-4">
       <div>
-        <h1>DIO-Github-app</h1>
+        <Link to={"/"}>
+          <h1>DIO-Github-app</h1>
+        </Link>
       </div>
       <div className="flex max-w-md w-full">
         <SearchBar />
