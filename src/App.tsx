4510fafad27da731d5803b1d6e8fe69169b6f455
ifilipe-lype <ifilipe.lype@gmail.com>
2022-03-11 12:09:51 +0100

    feat: adds routes for profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/header";
+import Profile from "./pages/profile";
+import { GithubContextProvider } from "./contexts/github";
 
 function App() {
   return (
-    <div className="flex justify-center">
-      <div className="max-w-4xl w-full flex flex-col items-center">
-        <Header />
+    <GithubContextProvider>
+      <div className="flex flex-col min-h-screen gap-16 items-center">
+        <div className="max-w-4xl w-full flex flex-col items-center">
+          <Header />
+        </div>
+        <div>
+          <Routes>
+            <Route path="/profile/:username" element={<Profile />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </GithubContextProvider>
   );
 }
 
