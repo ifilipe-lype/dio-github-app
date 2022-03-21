ec7d879708b1502ce6269383206acf11de7c724a
ifilipe-lype <ifilipe.lype@gmail.com>
2022-03-22 00:07:18 +0100

    feat: exports Bare home component page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Home(){
+    return (
+        <div className="py-8 bg-gray-100 flex w-full app-container rounded-lg">
+            <h1 className="text-xl text-gray-700 font-semibold">Search for users on github</h1>
+        </div>
+    );
+}
+
+export default Home;
