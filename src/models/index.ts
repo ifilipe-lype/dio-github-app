fab400170c2c73f17ae37c2da829de6a2cec9636
ifilipe-lype <ifilipe.lype@gmail.com>
2022-03-21 23:14:45 +0100

    feat: export IGithubRepo Interface

--- src/models/index.ts
+++ src/models/index.ts
@@ -25,3 +25,22 @@ export interface IGithubSearchUser{
 export interface IProfilePassingState {
     username: string;
 }
+
+export interface IGithubRepo {
+    id: string;
+    name: string;
+    full_name: string;
+    html_url: string;
+    description: string;
+    created_at: Date;
+    language: string;
+    license: {
+        key: string;
+        name: string;
+    };
+    topics: string[];
+    stargazers_count: number;
+    forks_count: number;
+    watchers: number;
+    homepage: string;
+}
